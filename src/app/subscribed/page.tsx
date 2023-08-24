a8ce8b51c8f4e7c50add98cc1151c1b8a09f558e
Nilufar shodmonova <nilufar4703@gmail.com>
2023-08-24 16:13:50 +0300

    feat: subscribed page responsive

--- src/app/subscribed/page.tsx
+++ src/app/subscribed/page.tsx
@@ -9,7 +9,7 @@ export default function SUbscribed() {
     <main className="flex flex-row">
       <SideBar />
 
-      <div className="grid xxl:grid-cols-4 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:gird-cols-1">
+      <div className="grid grid-rows-6 xxl:grid-cols-4 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:gird-cols-1">
         {videosData.map((video: VideoCardProps) => (
           <VideoCard
             id={video.id}
